0f5383d682a7329e40a7fe16812a4e66745f87aa
Kees Kluskens <kees@webduck.nl>
2016-10-01 15:40:01 +0200

    tweak directory structure

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 const POCKET_SAVE_URL = 'https://getpocket.com/save?url=';
 
 function saveUrl(rawUrl) {
-    const url = encodeURI(rawUrl);
-    chrome.tabs.create({
-        url: `${POCKET_SAVE_URL}${url}`,
-    });
+	const url = encodeURI(rawUrl);
+	chrome.tabs.create({
+		url: `${POCKET_SAVE_URL}${url}`,
+	});
 }
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    saveUrl(tab.url);
+	saveUrl(tab.url);
 });
