31df2408de3cf81a5e100fcda6c7f6ce2f2186c5
Kees Kluskens <kees@webduck.nl>
2017-03-07 23:59:03 +0100

    Save to pocket in background instead of with a new tab

--- src/main.js
+++ src/main.js
@@ -1,9 +1,33 @@
 const POCKET_SAVE_URL = 'https://getpocket.com/save?url=';
 
+function showNotification() {
+	chrome.notifications.create('added-to-pocket', {
+		type: 'basic',
+		iconUrl: 'img/128.png',
+		title: 'Saved to Pocket',
+		message: 'You have successfully saved this item to Pocket.',
+	}, () => {
+		setTimeout(() => {
+			chrome.notifications.clear('added-to-pocket');
+		}, 1500);
+	});
+}
+
 function saveUrl(rawUrl) {
 	const url = encodeURIComponent(rawUrl);
-	chrome.tabs.create({
-		url: `${POCKET_SAVE_URL}${url}`,
+	const fullUrl = `${POCKET_SAVE_URL}${url}`;
+
+	fetch(fullUrl, {
+		credentials: 'include',
+	})
+	.then(res => res.text())
+	.then((text) => {
+		if (text.includes('<title>Pocket: Add Item</title>')) {
+			showNotification();
+		} else {
+			// If saving the URL was unsuccessful, the user probably needs to login.
+			chrome.tabs.create({ url: fullUrl });
+		}
 	});
 }
 
